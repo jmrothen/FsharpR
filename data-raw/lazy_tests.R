b78c729f1755a0ceef7f47f8e4a2a9076acb9d34
John Rothen <rackjothen@gmail.com>
2023-06-11 16:22:41 -0400

    essentially scrap files, used for testing funcs real quick or testing addin-keyboard-shortcuts

--- data-raw/lazy_tests.R
+++ data-raw/lazy_tests.R
@@ -10,3 +10,11 @@ pp <- fsi_init()
 #find_fsi()
 
 to_fsi(code, show=T)
+
+rstudioapi::getActiveDocumentContext()
+
+let x = 1
+let y = 2
+x + y
+
+to_fsi
---
+++ data-raw/test.fs
@@ -0,0 +1,8 @@
+# this is essentially a test for the addins
+# highlight over the following test and execute it using the addin-shortcut that you can setup
+# i use ctrl+shift+alt+f
+
+
+let x = 1
+let y = 2
+x + y
